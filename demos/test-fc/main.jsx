e1ea59b498b9b4299953c701f208d1d4d30f24ed
huangqian <huangqian@vortextinfo.cn>
2025-06-19 20:09:42 +0800

    feat: add dev demo

---
+++ demos/test-fc/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+
+function App() {
+  return (
+      <div>Hello, World!</div>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <App />
+)
