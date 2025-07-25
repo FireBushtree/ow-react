2258e301effa0be2b776ed301e4e0cdee2fc1c0a
FireBushtree <firebushtree@163.com>
2025-07-25 23:04:57 +0800

    feat: refine project packages

---
+++ packages/demos/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="card">
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </div>
+  )
+}
+
+export default App
