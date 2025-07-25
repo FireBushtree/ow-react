60966db0eef1809c993f2bfec5930128cd7c3200
FireBushtree <firebushtree@163.com>
2025-07-25 22:23:43 +0800

    feat: init ts version

--- packages/shared/internals.js
+++
@@ -1,3 +0,0 @@
-import * as React from 'react'
-
-export default React.__SECRET_INTERNALS__OWEN
--- scripts/rollup/dev.config.js
+++
@@ -1,9 +0,0 @@
-import reactConfig from './react.config'
-import reactDomConfig from './react-dom.config'
-import sharedConfig from './shared.config'
-
-export default [
-  ...reactConfig,
-  ...reactDomConfig,
-  ...sharedConfig
-]
